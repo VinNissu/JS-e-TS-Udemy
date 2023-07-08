6a068a164381db31fcad64abd2a6e9004a098e0d
VinNissu <VinnyAlmeidabr@gmail.com>
2023-07-08 18:23:09 -0300

    9

--- 31.Funcoes(Aula17).js
+++ 31.Funcoes(Aula17).js
@@ -26,3 +26,20 @@ saudacao("Maria Cecilia")
 
 
 
+
+
+
+
+//retornando valor de função    
+function saudacao (nome){
+    console.log(`Bom dia ,${nome}!`)
+    return nome;
+}
+saudacao("Olá");
+
+let variavel = saudacao ("Luiz")
+console.log(variavel, " |valor retornado pelo return dentro da função")
+
+
+
+
