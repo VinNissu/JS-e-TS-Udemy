3ecbbb11411a22c0473f35801a03c1fa46cfc2fb
VinNissu <VinnyAlmeidabr@gmail.com>
2023-07-08 18:14:29 -0300

    8

---
+++ 31.Funcoes(Aula17).js
@@ -0,0 +1,28 @@
+//Funções em Javascript 
+
+
+//Criando uma função
+function saudacao () {
+    console.log('Bom dia!')
+
+}
+saudacao();
+
+
+
+//Criando parâmetro na função para dizer algo
+function saudacao (nome){
+    console.log(nome)
+}
+saudacao("Olá");
+
+//Criando parâmetro na função para dizer algo com cráse
+function saudacao (nome){
+    console.log(`Bom dia ${nome}!`)
+}
+saudacao("Olá");
+saudacao("Vinícius")
+saudacao("Maria Cecilia")
+
+
+
