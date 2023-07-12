222a71efd43458ce46b410cf9084dc4988ebdf8a
Vinícius Luiz <110014684+VinNissu@users.noreply.github.com>
2023-07-12 12:57:43 -0300

    21

--- 34.(Aula20)/js/34.(Aula20).js
+++ 34.(Aula20)/js/34.(Aula20).js
@@ -27,37 +27,83 @@
 
 
 
+
+//--1-Função que pega dados do "FORM" no HTML com query selector 
 function meuEscopo(){
-    let form = document.querySelector('.form');
+    let form = document.querySelector('.form'); 
+    //Pegando o Form do HTML
+
     let resultado = document.querySelector('.resultado');
+    //Pegando o resultado do HTML 
+    
+    let replicaResultado = document.querySelector('.replicaResultado')
     
     let pessoas = [];
+    //Criando um array
+    
+    
+    
+    //----------------------------------------------------------------------------------------------------------
+    let pessoasTest = [];
 
-    function recebeEventoForm(evento){
+    function testeEvento(evento){
+
+
         evento.preventDefault();
-
         let nome = form.querySelector('.nome');
-        let sobrenome = form.querySelector('.sobrenome');
-        let peso = form.querySelector('.peso');
-        let altura = form.querySelector('.altura');
 
-        pessoas.push({
-            nome: nome.value,
-            sobrenome: sobrenome.value,
-            peso: peso.value,
-            altura: altura.value
-        });
 
-        console.log(nome.value, sobrenome.value, peso.value, altura.value);
-        console.log(pessoas)
+        pessoasTest.push =({nome: nome.value});
 
-        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+        testeEvento.innerHTML =+ `<h1> teste ${nome}</h1>`
+        
+        console.log(nome);  
+        console.log(pessoasTest);  
     }
 
-    form.addEventListener('submit', recebeEventoForm);
+    //----------------------------------------------------------------------------------------------------------
+
+    //Função para que vai pegar os dados do HTML
+    function recebeEventoForm(evento){
+
+        evento.preventDefault();
+        //Colocando o parâmetro e o preventDefault() para não atualizar a página.
+
+        let nome = form.querySelector('.nome');
+        //criando variável "nome" e pegando valor com QuerySelector do class "nome" no HTML.
+
+        let sobrenome = form.querySelector('.sobrenome');
+         //criando variável "sobrenome" e pegando valor com QuerySelector do class "sobrenome" no HTML.    
+
+        let peso = form.querySelector('.peso');
+        //criando variável "peso" e pegando valor com QuerySelector do class "peso" no HTML.
+
+        let altura = form.querySelector('.altura');
+        //criando variável "altura" e pegando valor com QuerySelector do class "altura" no HTML.
+
+
+        //Usando o "Array" (pessoas) + push para adicionar nele mesmo os valores nome.value,sobrenome.value,peso.value,altura.value nos campos nome,sobrenome,peso,altura
+        pessoas.push({
+            nome: nome.value, 
+            //Ele está pegando o valor "nome" do "HTML" que vem do HTML e colocando na variável nome.
+            sobrenome: sobrenome.value,
+            //Ele está pegando o valor "sobrenome" do "HTML" com o sobrenome.value e colocando na variável sobrenome.
+            peso: peso.value,
+            //Ele está pegando o valor "peso" do "HTML" com o sobrenome.value e colocando na variável peso.
+            altura: altura.value
+            //Ele está pegando o valor "altura" do "HTML" com o altura.value e colocando na variável peso.
+        });
+        console.log('Campos que vem do FORM do HTML >',nome.value, sobrenome.value, peso.value, altura.value);
+        console.log('O array que receberá as informações',pessoas)
+
+
+        resultado.innerHTML += `<p>Nome:${nome.value} Sobrenome:${sobrenome.value} Peso:${peso.value} Altura:${altura.value}</p>`;
+        //Com o innerHTML, o JS está inserindo esses campos no HTML com o class "resultado".
+
+    form.addEventListener('submit', testeEvento);
+    //O AddEventListener está no "form" esperando o "submit" para depois executar o "recebeEventoForm"
 };
 meuEscopo();
+testeEvento();
 
-
-
-
+}
